60e42c8723f049e4d501b419217f779f7a03c7c7
M0stafaRady <mostafa.rady@efabless.com>
2025-01-13 17:56:09 +0200

    run documentation with latest buswrap updates

--- hdl/rtl/EF_QSPI_XIP_CTRL.v
+++ hdl/rtl/EF_QSPI_XIP_CTRL.v
@@ -1,5 +1,5 @@
 /*
-	Copyright 2020 Mohamed Shalan (mshalan@aucegypt.edu)
+	Copyright 2020 Efabless Corp (ip_admin@efabless.com)
 	
 	Licensed under the Apache License, Version 2.0 (the "License"); 
 	you may not use this file except in compliance with the License. 
